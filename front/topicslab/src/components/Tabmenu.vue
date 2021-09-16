7b6d1462503ecdae9cecd41b961537c429aad376
伊舎堂 永 <17301@toba.kosen-ac.jp>
2021-09-16 11:35:07 +0900

    25まだです

--- front/topicslab/src/components/Tabmenu.vue
+++ front/topicslab/src/components/Tabmenu.vue
@@ -2,7 +2,16 @@
   <div>
     <TabView>
       <TabPanel header="トピック">
-        トピック
+    <Card v-for="user in users" :key="user.id">
+        <template #content>
+          <h2>
+            <router-link :to="`/topic/${user.id}`">
+              {{ user.name }}
+            </router-link>
+          </h2>
+        </template>
+    </Card>
+    <Skeleton v-if="seen" height="150px" />
       </TabPanel>
       <TabPanel header="コメント">
         コメント
@@ -12,5 +21,38 @@
 </template>
 
 <script>
+import axios from '@/supports/axios'
 
+export default {
+  name: 'Tabmenu',
+  data () {
+    return {
+      users: [],
+      seen: true
+    }
+  },
+  mounted () {
+    this.getTabmenu()
+  },
+  methods: {
+    getTabmenu () {
+      axios.get('/sanctum/csrf-cookie')
+        .then(() => {
+          axios.get('/api/user')
+            .then((res) => {
+              if (res.status === 200) {
+                this.users.splice(0)
+                this.users.push(res.data)
+                this.seen = false
+              } else {
+                console.log('取得失敗')
+              }
+            })
+        })
+        .catch((err) => {
+          alert(err)
+        })
+    }
+  }
+}
 </script>
